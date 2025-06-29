743322a9718a506d5b621ea9540e5415a1a491d6
Jessica De-Paz <T0141000@miamidade.gov>
2025-06-29 04:28:52 -0400

    final fcfs.c

--- PA2/pa2 draft/fcfs.c
+++ PA2/pa2 draft/fcfs.c
@@ -3,35 +3,31 @@
 void simulate_fcfs() {
     int t = 0;
     int completed = 0;
-	//queue ready[10];
-	//for(int i = 0; i < 10;i++)
-		//ready[i] = init_queue();
 	queue ready = init_queue();
+
     while (completed < n) {
-		//char* str = to_str(ready);
-		//printf("Ready queue @ t = %d: %s\n", t, str);
-		//free(str);
+		// Enqueue newly arrived processes
 		for (int i = 0; i < n; i++) {
 			if (processes[i].arrival_time == t){ 
 				processes[i].active = 1;
 				enqueue(&ready, processes[i]);
 			}
 		}
+		// If no active process is in the ready queue, increment time
         if (!ready.size) {
             t++;
             continue;
         }
+		// Run the process at the front of the queue
 		int cur = peek(ready).id;
         processes[cur].remaining_time--;
         t++;
+		// If process finishes
         if (processes[cur].remaining_time == 0) {
             processes[cur].active = 0;
             processes[cur].turnaround_time = t - processes[cur].arrival_time;
             completed++;
 			dequeue(&ready);
-			//printf("p%d arrived at %d, got terminated at %d, with TT = %d.\n",
-			//	processes[cur].id, processes[cur].arrival_time,
-			//	t, processes[cur].turnaround_time);
         }
     }
 }
