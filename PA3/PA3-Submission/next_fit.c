f01c0068506c50ce21ddbdb96113768ce7a1c61a
leochirinos10 <leonardoenriquechirinos@gmail.com>
2025-07-20 21:49:45 -0400

    commented out print statements for next fit

--- PA3/PA3-Submission/next_fit.c
+++ PA3/PA3-Submission/next_fit.c
@@ -35,32 +35,32 @@ static int find_next_fit(int request_size){
     return current_addr; // Address of the first hole whose size is appropriate
 }
 
-static void print_all_blocks(int count){
-    for(int i = 0; i < count;i++)
-        printf("block[%d]: %s, %d, [%d, %d]\t",
-            i, blocks[i].released?"released":"occupied",
-            blocks[i].size,
-            blocks[i].address,
-            blocks[i].address + blocks[i].size + 3);
-    putchar('\n');
-}
+// static void print_all_blocks(int count){
+//     for(int i = 0; i < count;i++)
+//         //printf("block[%d]: %s, %d, [%d, %d]\t",
+//             i, blocks[i].released?"released":"occupied",
+//             blocks[i].size,
+//             blocks[i].address,
+//             //blocks[i].address + blocks[i].size + 3);
+//     putchar('\n');
+// }
 
-static void print_all_holes(){
-    int hole = 0, i = 0;
-    do{
-        printf("hole#%d: %d, [%d, %d], P=%d, N=%d\t",
-            i++,
-            -memory[hole],
-            hole, hole - memory[hole] + 3,
-            memory[hole + PREV],
-            memory[hole + NEXT]);
-            hole = memory[hole + NEXT];
-    }while(hole);
-    putchar('\n');
-}
+// static void print_all_holes(){
+//     int hole = 0, i = 0;
+//     do{
+//         //printf("hole#%d: %d, [%d, %d], P=%d, N=%d\t",
+//             i++,
+//             -memory[hole],
+//             hole, hole - memory[hole] + 3,
+//             memory[hole + PREV],
+//             //memory[hole + NEXT]);
+//             hole = memory[hole + NEXT];
+//     }while(hole);
+//     putchar('\n');
+// }
 
 void simulate_next_fit(){
-    printf("Simulating next-fit!\n");
+    //printf("Simulating next-fit!\n");
     memory[0] = -(n-4);
     memory[n-1] = -(n-4);
     memory[1] = memory[2] = 0;
@@ -72,9 +72,9 @@ void simulate_next_fit(){
     int i = 0;
     for(int round = 0; round < x; round++){
         while(1){
-            printf("Trying to satisfy request(%d)...", next_request[i]);
+            //printf("Trying to satisfy request(%d)...", next_request[i]);
             int hole_addr = find_next_fit(next_request[i]);
-            printf("\thole address: %d\n", hole_addr);
+            //printf("\thole address: %d\n", hole_addr);
             if(hole_addr < 0)
             break;
             int block_size = next_request[i++];
@@ -82,21 +82,21 @@ void simulate_next_fit(){
             total_occupied_size += block_size;
         }
         amu += (total_occupied_size/(double)n);
-        printf("current AMU: %.2f%%.\n", 100*amu/(round+1));
+        // printf("current AMU: %.2f%%.\n", 100*amu/(round+1));
         int to_be_released_block;
         do{//randomly choose an occupied block to release it
             to_be_released_block = rand() % block_count;
         }
         while(blocks[to_be_released_block].released);
-        printf("block#%d will be released.\n", to_be_released_block);
-        printf("Before release:\n");
-        print_all_blocks(block_count);
-        print_all_holes();
+        // printf("block#%d will be released.\n", to_be_released_block);
+        // printf("Before release:\n");
+        // print_all_blocks(block_count);
+        // print_all_holes();
         release(to_be_released_block);
         total_occupied_size -= blocks[to_be_released_block].size;
-        printf("After release:\n");
-        print_all_blocks(block_count);
-        print_all_holes();
+        // printf("After release:\n");
+        // print_all_blocks(block_count);
+        // print_all_holes();
     }
     printf("Average memory utilization for next fit is %.2f%%.\n",
         100 * amu/x);
